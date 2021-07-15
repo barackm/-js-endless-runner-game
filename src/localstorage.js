84809e03c1cdb723c04c0115a91f6cbc7c04f470
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-15 20:34:26 +0200

    Finish local storage stuffs

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,2 +1,18 @@
-const storage = (() => {})();
+const storage = (() => {
+  const get = (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  };
+  const set = (key, value) => {
+    return localStorage.setItem(key, JSON.parse(value));
+  };
+
+  const remove = (key) => {
+    return localStorage.removeItem(key);
+  };
+  return {
+    get,
+    set,
+    remove,
+  };
+})();
 export default storage;
